bef7d219a3ad0e0bf695e46f260e2394ab0100a3
Gosaye White <113490286+GosayeWhite@users.noreply.github.com>
2023-06-08 14:52:40 -0400

    Final Analysis

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "d8eb4b8c416db5d5965f233adc289682"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "6ca6d5ffbbd44c0e9922f25198796705"
